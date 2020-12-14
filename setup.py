d0f045a581dc6592d3affa18811f3c791af4e2af
helisoya <30603834+helisoya@users.noreply.github.com>
2020-12-14 17:15:26 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="Harold's Quest1 ",
+version = '1.0',
+    packages = [''],
+    url = '',
+    icense = '',
+    author = 'PC',
+    author_email = '',
+    description = ''
+)
